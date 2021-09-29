09b28928f97a0478c24e34c7eac34e79a7a8be52
MarkBezsonov <markbezsonov@gmail.com>
2021-09-29 16:11:00 -0500

    Create Stock model/attriibutes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_002336) do
+ActiveRecord::Schema.define(version: 2021_09_29_210806) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
